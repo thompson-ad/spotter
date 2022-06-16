db50e1e907cbc24020132cdde4121eec03be07f6
Aaron Thompson <thompson_ad@outlook.com>
2022-06-16 07:16:23 +0100

    add example fixture

--- fixtures.ts
+++ fixtures.ts
@@ -1,1 +1,36 @@
-export const data = [];
+export const data = [
+  {
+    movement: "Bench Press",
+    progressions: [
+      {
+        date: "dateString",
+        weight: 10,
+        reps: 4,
+        set: 3,
+      },
+      {
+        date: "dateString",
+        weight: 20,
+        reps: 4,
+        set: 3,
+      },
+    ],
+  },
+  {
+    movement: "Deadlift",
+    progressions: [
+      {
+        date: "dateString",
+        weight: 10,
+        reps: 4,
+        set: 3,
+      },
+      {
+        date: "dateString",
+        weight: 20,
+        reps: 4,
+        set: 3,
+      },
+    ],
+  },
+];
